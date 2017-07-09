5dbc67776ea15878972d25d15127223ce27829c9
KIM Taegyoon <steloflute@gmail.com>
2017-07-09 23:05:11 +0900

    typeful

--- korbit-ticker.go
+++ korbit-ticker.go
@@ -24,15 +24,20 @@ func slurp(url string) ([]byte, error) {
 	return body, nil
 }
 
-func getQuote(currencyPair string) (int64, string, error) {
+type quote struct {
+	Timestamp int64
+	Last string
+}
+
+func getQuote(currencyPair string) (quote, error) {
 	// {"timestamp":1499504423000,"last":"292000"}
 	s, err := slurp("https://api.korbit.co.kr/v1/ticker?currency_pair=" + currencyPair)
+	var m quote
 	if err != nil {
-		return 0, "", err
+		return m, err
 	}
-	var m map[string]interface{}
 	err = json.Unmarshal(s, &m)
-	return int64(m["timestamp"].(float64)), m["last"].(string), nil
+	return m, nil
 }
 
 func main() {
@@ -40,11 +45,11 @@ func main() {
 	fmt.Println("Refreshes every", interval, "minutes.")
 	for {
 		fmt.Println(time.Now())
-		for _, v := range []string{"btc_krw", "eth_krw", "etc_krw", "xrp_krw"} {
-			if t, p, err := getQuote(v); err != nil {
+		for _, currencyPair := range []string{"btc_krw", "eth_krw", "etc_krw", "xrp_krw"} {
+			if q, err := getQuote(currencyPair); err != nil {
 				fmt.Println(err)
 			} else {
-				fmt.Println(v, ":", time.Unix(t / 1000, 0), p)
+				fmt.Println(currencyPair, ":", time.Unix(q.Timestamp / 1000, 0), q.Last)
 			}
 		}
 		time.Sleep(interval * time.Minute)
